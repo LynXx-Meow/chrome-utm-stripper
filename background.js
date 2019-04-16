13ac0f0af8da1d6badceb4c288c480dc9a186c53
LynXx-Meow <jonathan.biberian@free.fr>
2019-04-16 17:43:13 +0200

    More tracking tokens
    Added HumbleBundle, MailChimp, other-xtref and other-icid

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
-var utm_re = new RegExp('([\?\&](mkt_tok|(g|fb)clid|utm_(source|medium|term|campaign|content|cid|reader|referrer|name))=[^&#]+)', 'ig');
+var utm_re = new RegExp('([\?\&](mkt_tok|(g|fb)clid|utm_(source|medium|term|campaign|content|cid|reader|referrer|name)|xtref|hmb_(source|medium|campaign)|mc_(c|e)id|icid)=[^&#]*)', 'ig');
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    var url = details.url;
-    var queryStringIndex = url.indexOf('?');
-    if (url.indexOf('utm_') > queryStringIndex ||
-      url.indexOf('clid') > queryStringIndex ||
-      url.indexOf('mkt_tok') > queryStringIndex) {
-        var stripped = url.replace(utm_re, '');
-        if (stripped.charAt(queryStringIndex) === '&') {
-            stripped = stripped.substr(0, queryStringIndex) + '?' +
-                stripped.substr(queryStringIndex + 1)
-        }
-        if (stripped != url) {
-            return {redirectUrl: stripped};
-        }
-    }
+browser.webRequest.onBeforeRequest.addListener(function(details) {
+	var url = details.url;
+	var queryStringIndex = url.indexOf('?');
+	if (url.indexOf('utm_') > queryStringIndex ||
+		url.indexOf('clid') > queryStringIndex ||
+		url.indexOf('mkt_tok') > queryStringIndex ||
+		url.indexOf('xtref') > queryStringIndex ||
+		url.indexOf('hmb_') > queryStringIndex ||
+		url.indexOf('mc_') > queryStringIndex ||
+		url.indexOf('icid') > queryStringIndex) {
+		var stripped = url.replace(utm_re, '');
+		if (stripped.charAt(queryStringIndex) === '&') {
+			stripped = stripped.substr(0, queryStringIndex) + '?' +
+				stripped.substr(queryStringIndex + 1)
+		}
+		if (stripped != url) {
+			return {redirectUrl: stripped};
+		}
+	}
 },
 {urls: ['https://*/*?*', 'http://*/*?*'], types: ['main_frame']}, ['blocking']);
